5cb434a872c62639bf456ea05a09452931b907e5
SuneetBansal <32817920+SuneetBansal@users.noreply.github.com>
2023-12-10 16:12:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('TEST RPPO');
+
+arr = [{
+  name: 'a',
+  val: 1
+}, {
+  name: 'b',
+  val: 2
+}, {
+  name: 'c',
+  val: 3
+}]
+
+
+console.log(getVal(arr, 'a'));
+
+
+function getVal(arr, v) {
+  for (i=0; i<arr.length;i++) {
+    if (arr[i]?.name === v) {
+      return arr[i]  
+    }
+  }  
+}
